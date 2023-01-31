18891865aa8280e1c049ed60652d4f8d97092309
Mohanraj1100 <123471382+Mohanraj1100@users.noreply.github.com>
2023-01-31 12:12:45 +0530

    change

--- src/main/java/assignment/school/Student.java
+++ src/main/java/assignment/school/Student.java
@@ -1,58 +1,73 @@
 package assignment.school;
-import java.io.*;
-import java.lang.*;
 import java.util.*;
+import java.util.logging.*;
 public class Student 
 {
-	String StudentName;
-    char GradeLevel;
-    float GPA;
-    float UpdatedGPA;
-    String Response;
+    String studentname;
+    char gradelevel;
+    float gpa;
+    float updatedgpa;
+    String response;
     Scanner sc = new Scanner(System.in);
     Scanner si = new Scanner(System.in);
+    Logger log = Logger.getLogger("hi");
 
     Student() {
-        System.out.println("Enter the Student Name");
-        StudentName = sc.nextLine();
-        System.out.println("Enter the Grade level");
-        GradeLevel = sc.next().charAt(0);
-        System.out.println("Enter your GPA out of 10");
-        GPA = sc.nextFloat();
+	try{
+        log.info("Enter the Student Name");
+        studentname = sc.nextLine();
+        log.info("Enter the Grade level");
+        gradelevel = sc.next().charAt(0);
+        log.info("Enter your GPA out of 10");
+        gpa = sc.nextFloat();
+	}
+	catch(InputMismatchException e)
+	{
+		log.info(String.valueOf(e));
+		System.exit(0);
+	}
+	    
     }
 
-    void Update() {
-        System.out.println("Do you need to update your GPA\nFor update type Yes");
-        Response = si.nextLine();
-        switch (Response) {
+    void update() {
+        log.info("Do you need to update your GPA\nFor update type Yes");
+        response = si.nextLine();
+        switch (response) {
             case "Yes":
-                System.out.println("Enter updated GPA");
-                UpdatedGPA = si.nextFloat();
+		try{	
+                log.info("Enter updated GPA");
+                updatedgpa = si.nextFloat();
                 if ((GradeLevel == 'A') && (UpdatedGPA >= 9) && (UpdatedGPA <= 10)) {
-                    GPA = UpdatedGPA;
+                    gpa = updatedgpa;
                 } else if ((GradeLevel == 'B') && (UpdatedGPA >= 7) && (UpdatedGPA <= 8)) {
-                    GPA = UpdatedGPA;
+                    gpa = updatedgpa;
                 } else if ((GradeLevel == 'C') && (UpdatedGPA >= 5) && (UpdatedGPA <= 6)) {
-                    GPA = UpdatedGPA;
+                    gpa = updatedgpa;
                 } else if ((GradeLevel == 'D') && (UpdatedGPA >= 3) && (UpdatedGPA <= 4)) {
-                    GPA = UpdatedGPA;
+                    gpa = updatedgpa;
                 } else if ((GradeLevel == 'E') && (UpdatedGPA >= 1) && (UpdatedGPA <= 2)) {
-                    GPA = UpdatedGPA;
+                    gpa = updatedgpa;
                 } else {
-                    System.out.println("Cannot be updated");
+                    log.info("Cannot be updated");
                 }
+		}
+		catch(InputMismatchException e)
+		{
+			log.info(String.valueOf(e);
+			System.exit(0);
+		}				
                 break;
         }
     }
 
-    void Result() {
-        System.out.println(StudentName + " has a GPA Level of" + GPA);
+    void result() {
+        log.fine("{} has a GPA Level of {}",studentname,gpa);
     }
 
     public static void main(String[] args) {
         Student in = new Student();
-        in.Update();
-        in.Result();
+        in.update();
+        in.result();
     }
 
 }
