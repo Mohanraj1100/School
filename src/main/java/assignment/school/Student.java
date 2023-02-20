1d579b46e7d7c38594b4579477de3e755b9be07a
Mohanraj1100 <123471382+Mohanraj1100@users.noreply.github.com>
2023-02-20 23:14:45 +0530

    commit

--- src/main/java/assignment/school/Student.java
+++ src/main/java/assignment/school/Student.java
@@ -3,57 +3,67 @@ import java.util.*;
 import java.util.logging.*;
 public class Student 
 {
-    String studentname;
+    public static void main(String[] args) {
+    	String studentname;
+        char gradelevel;
+        float gpa;
+        Scanner inputobj = new Scanner(System.in);
+        Logger log = Logger.getLogger("hi");
+        log.info("Enter the Student Name");
+        studentname = inputobj.nextLine();
+        log.info("Enter the Grade level");
+        gradelevel = inputobj.next().charAt(0);
+        log.info("Enter your GPA out of 10");
+        gpa = inputobj.nextFloat();
+        StudentLevel sl = new StudentLevel(studentname,gradelevel,gpa);
+        sl.update();
+        sl.result();
+    }
+
+}
+
+class StudentLevel{
+	String studentname;
     char gradelevel;
     float gpa;
     float updatedgpa;
     int response;
-    Scanner sc = new Scanner(System.in);
-    Scanner si = new Scanner(System.in);
+    Scanner inputobj = new Scanner(System.in);
     Logger log = Logger.getLogger("hi");
-
-    Student() {
-            log.info("Enter the Student Name");
-            studentname = sc.nextLine();
-            log.info("Enter the Grade level");
-            gradelevel = sc.next().charAt(0);
-            log.info("Enter your GPA out of 10");
-            gpa = sc.nextFloat();
+    StudentLevel(String studentname,char gradelevel,float gpa)
+    {
+    	this.studentname = studentname;
+    	this.gradelevel = gradelevel;
+    	this.gpa = gpa;
     }
-
+    
     void update() {
-            log.info("Do you need to update your GPA\nFor update type 1");
-            response = si.nextInt();
-            if(response == 1) {
-                log.info("Enter updated GPA");
-                updatedgpa = si.nextFloat();
-                if ((gradelevel == 'A') && (updatedgpa >= 9) && (updatedgpa <= 10)) {
-                    gpa = updatedgpa;
-                } else if ((gradelevel == 'B') && (updatedgpa >= 7) && (updatedgpa <= 8)) {
-                    gpa = updatedgpa;
-                } else if ((gradelevel == 'C') && (updatedgpa >= 5) && (updatedgpa <= 6)) {
-                    gpa = updatedgpa;
-                } else if ((gradelevel == 'D') && (updatedgpa >= 3) && (updatedgpa <= 4)) {
-                    gpa = updatedgpa;
-                } else if ((gradelevel == 'E') && (updatedgpa >= 1) && (updatedgpa <= 2)) {
-                    gpa = updatedgpa;
-                } else {
-                    log.info("Cannot be updated");
-                }		
-        }
-        else{
-            log.info("Invalid");
-        }
+        log.info("Do you need to update your GPA\nFor update type 1");
+        response = inputobj.nextInt();
+        if(response == 1) {
+            log.info("Enter updated GPA");
+            updatedgpa = inputobj.nextFloat();
+            if ((gradelevel == 'A') && (updatedgpa >= 9) && (updatedgpa <= 10)) {
+                gpa = updatedgpa;
+            } else if ((gradelevel == 'B') && (updatedgpa >= 7) && (updatedgpa <= 8)) {
+                gpa = updatedgpa;
+            } else if ((gradelevel == 'C') && (updatedgpa >= 5) && (updatedgpa <= 6)) {
+                gpa = updatedgpa;
+            } else if ((gradelevel == 'D') && (updatedgpa >= 3) && (updatedgpa <= 4)) {
+                gpa = updatedgpa;
+            } else if ((gradelevel == 'E') && (updatedgpa >= 1) && (updatedgpa <= 2)) {
+                gpa = updatedgpa;
+            } else {
+                log.info("Cannot be updated");
+            }		
     }
-
-    void result() {
-        log.log(Level.INFO,()-> studentname +" has a GPA Level of "+gpa);
+    else{
+        log.info("Invalid");
     }
+}
 
-    public static void main(String[] args) {
-        Student in = new Student();
-        in.update();
-        in.result();
-    }
-
+void result() {
+    log.log(Level.INFO,()-> studentname +" has a GPA Level of "+gpa);
+}
+	
 }
